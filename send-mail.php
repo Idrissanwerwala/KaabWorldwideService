8311c256da397d803e95feb040bce22cd259a482
Idrissanwerwala <157010832+Idrissanwerwala@users.noreply.github.com>
2026-02-09 15:33:46 +0530

    Remove hardcoded SMTP credentials from send-mail.php
    Removed sensitive SMTP configuration details.

--- send-mail.php
+++ send-mail.php
@@ -27,14 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'idrissawerwala@gmail.com';
-        $mail->Password   = 'lffkneyohcxpjfwt'; // App Password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port       = 587;
-
-        // Mail setup
-        $mail->setFrom('idrissawerwala@gmail.com', 'Website Contact');
-        $mail->addAddress('idrissawerwala@gmail.com');
+       
         $mail->addReplyTo($email);
 
         $mail->isHTML(false);
